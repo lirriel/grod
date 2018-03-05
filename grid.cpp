8a88eb1d03a612704dd1aa6c3e5456e24556289a
lirriel <druidmif@gmail.com>
2018-03-05 20:43:41 +0300

    Initial commit

---
+++ grid.cpp
@@ -0,0 +1,150 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+/// \file
+/// \brief Contains implementation of \a grid class
+///
+/// Should be augmented by students if needed
+///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>      // std::setw() if needed
+#include <stdexcept>
+
+#include "grid.h"
+
+// According to new restrictions of C++ standards, an explicit definition of static consts is a need
+const bool grid::NOT_INFECTED = false;      // = false;
+const bool grid::INFECTED = true;          // = true;
+
+
+
+// Students may need to alter the constructor
+grid::grid(const std::string& fileName)
+{
+  
+    std::ifstream grid_file;
+
+    try
+    {
+        grid_file.open(fileName.c_str());
+
+        // check if it's all ok
+        if (!grid_file.good())
+            throw std::runtime_error("File cannot be open");
+
+        // opened correctly
+        grid_file >> _rows;
+        grid_file >> _cols;
+
+        _area = std::vector<bool>(_rows * _cols, grid::NOT_INFECTED);
+        _visited = std::vector<bool>(_rows * _cols, false);
+
+        int blob_row;
+        int blob_col;
+
+        while (!grid_file.eof() && grid_file.good())
+        {
+            grid_file >> blob_row;
+            grid_file >> blob_col;
+
+            _area.operator[](indexof(blob_row, blob_col)) = INFECTED;
+        }
+    }
+    catch (...)
+    {
+        // always tries to properly close the file!
+        grid_file.close();
+        throw;              // dispatch further
+    }
+  
+    grid_file.close();
+}
+
+// Students may need to alter the destructor
+grid::~grid () 
+{ }
+
+
+// Students will need to alter this function to display the
+// plus signs (+) next to the cells that belong to
+// a counted colony.
+std::ostream& operator << (std::ostream& stream, const grid& ob) 
+{
+
+    for (int row = 0; row < ob._rows; ++row) 
+    {     
+        for (int col=0; col < ob._cols; col++) 
+        {
+            stream << ob._area.operator[](ob.indexof(row, col));
+            // adds + to unit we are looking for
+            stream << (ob._visited.operator[](ob.indexof(row, col)) ? "+   " : "    ");
+        }
+
+        stream << std::endl;
+    }
+    stream << std::endl;
+    
+    return stream;
+}
+
+bool grid::visited(int row, int col) const
+{
+    if (checkIndex(row, col))
+        return _visited.operator[](indexof(row, col));
+}
+
+
+// Students have to replace the return statement in this function with their
+// recursive implementation of this method */
+int grid::count (int row, int col)
+{
+    if (!checkIndex(row, col) || !infected(row, col))
+        return 0;
+
+    int sum = 0;
+
+    // go through all cells around the given one
+    for (int i = row - 1; i <= row + 1 && i < _rows; ++i)
+        for (int j = col - 1; j <= col + 1 && j < _cols; ++j)
+            if ((row != i || col != j) && checkIndex(i, j) && infected(i, j) && !visited(i, j))
+            {
+                _visited[indexof(i, j)] = true;
+                sum += count(i, j) + 1;
+            }
+
+    return sum;
+}
+
+
+// Students may need to alter this function
+int grid::getCount (int row, int col)
+{
+    return count(row,col);
+}
+
+
+
+// Students do not need to alter function indexof.
+int grid::indexof(int row, int col) const
+{
+    if (!checkIndex(row, col))
+        throw std::invalid_argument("Index out of range");
+
+    return row *_cols + col;
+}
+
+// Students do not need to alter function infected.
+bool grid::infected(int row, int col) const 
+{
+    return (_area.at((unsigned int) indexof(row, col)) == INFECTED);
+}
+
+
+// Students do not need to alter this function w/o any special reasons.
+bool grid::checkIndex(int row, int col) const
+{
+    if (row >= _rows || row < 0 || col >= _cols || col < 0)
+        return false;
+    return true;
+}
---
+++ grid.h
@@ -0,0 +1,102 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+/// \file
+/// \brief Contains definition of \a grid class
+///
+/// Should be augmented by students if needed
+///
+///////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef XI_ASSESSING_INFECTION_GRID_H
+#define XI_ASSESSING_INFECTION_GRID_H
+
+#include <string>
+#include <vector>
+
+ 
+
+/** \brief Represents a tissue sample as a rectangle grid structure
+ *
+ *  **IMPORTANT NOTE FOR STUDENTS**:
+ *
+ *  For this assignment, you might need to add state to the 
+ *  class and/or augment existing methods, and/or create private helper 
+ *  methods, but you should not declare new public methods
+ */
+class grid 
+{
+public:
+    static const bool NOT_INFECTED;         ///< it's clear!
+    static const bool INFECTED;              ///< Oops! someone needs to practice a bit safer.. programming
+
+public:
+    /** \brief Initializes with a file name
+     *
+     *  If the constructor fails to open a file, a \a std::runtime_error is raised
+     *
+     *  **Students may need** to alter the constructor
+     */
+    grid(const std::string& fileName);
+    
+    /** \brief Destructor
+     *
+     *  **Students may need** to alter the destructor
+     */
+    ~grid();
+
+    /** \brief Returns a count units for the area determined by coordinates (\a row, \a col) */
+    int getCount(int row, int col);
+    
+public:
+
+    /** \brief Operator << overloading
+     *
+     *  **Students will need** to alter this function to display the
+     *  plus signs (+) next to the cells that belong to
+     *  a counted colony.
+     */
+    friend std::ostream& operator<<(std::ostream& stream, const grid& ob);
+
+    
+    
+private:    
+    /** \brief Calculates a number of units for the cluster determined by coordinates (\a row, \a col) 
+     *  \returns the cluster size 
+     *
+     *  This routine should use a divide and conquer recursive approach to solve the problem. 
+     *  This means you will have two or more recursive calls in function \a grid::count(). 
+     *  To correctly complete function \a grid::count(), **student will have** to add additional 
+     *  data members and/or functions to class grid.
+     */
+    int count(int row, int col);
+
+    /** \brief Return the number of units for a given cell (\a row, \a col)
+     *
+     *  Students do not need to alter the function
+     */
+    int indexof(int row, int col) const;
+    
+    bool checkIndex(int row, int col) const;        ///< returns true, if a given index is in a range, false otherwise
+
+    /** \brief Returns true, if a given cell (\a row, \a col) is infected, false otherwise
+     *
+     *  Students do not need to alter the function
+     */
+    bool infected(int row, int col) const;
+
+    /** \brief Returns true if a cell (\a row, \a col) was checked already, false otherwise
+     *
+     *  Student created this function
+     */
+    bool visited(int row, int col) const;
+
+private:
+    std::vector<bool> _area;        ///< Stores dynamically created vector that holds the grid
+    int _rows;                      ///< Stores grid's rows number
+    int _cols;                      ///< Stores grid's columns number
+    std::vector<bool> _visited;     ///< Stores created vector that holds visited points
+}; // class grid
+
+
+
+#endif // XI_ASSESSING_INFECTION_GRID_H
